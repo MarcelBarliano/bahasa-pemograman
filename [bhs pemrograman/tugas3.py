6fcf8201bb14e5a9339be2ab48793d8d0892a15e
MarcelBarliano <marcel.barliano.03@gmail.com>
2022-10-11 21:45:20 +0700

    tugas3

---
+++ [bhs pemrograman/tugas3.py
@@ -0,0 +1,42 @@
+# OPERATOR ARITMATIKA
+
+#Penjumlahan
+print(13 + 2)
+semangka = 7
+melon = 9
+buah = semangka + melon #
+print(buah)
+
+#Pengurangan
+uanganda= 10000
+kembalian = 5000
+sisaKembalian = uanganda - kembalian
+print("Sisa kembalian Anda adalah ", sisaKembalian)
+
+#Perkalian
+panjang = 15
+lebar = 8
+luas = panjang * lebar
+print(luas)
+
+#Pembagian
+roti = 16
+anak = 4
+rotiPerAnak = roti / anak
+print("Setiap anak akan mendapatkan bagian roti sebanyak ", rotiPerAnak)
+
+#Sisa Bagi / Modulus
+bilangan1 = 14
+bilangan2 = 5
+hasil = bilangan1 % bilangan2
+print("Sisa bagi dari bilangan ", bilangan1, " dan ", bilangan2, " adalah ", hasil)
+
+#Pangkat
+bilangan3 = 8
+bilangan4 = 2
+hasilPangkat = bilangan3 ** bilangan4
+print(hasilPangkat)
+
+#Pembagian Bulat
+print(10//3) 
+#10 dibagi 3 adalah 3.3333. Karena dibulatkan maka akan menghasilkan nilai 3
